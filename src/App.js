ac3a2169575aa7fcc94926a565cad46736d74b12
James Perrin <countrydawgg@gmail.com>
2022-07-05 07:32:53 -0700

    Completed Task 3 - Part 2: Create the Footer component and modify App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import CampsiteList from './features/campsites/CampsiteList';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
 			<CampsiteList />
+			<Footer />
 		</div>
 	);
 }
