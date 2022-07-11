d435cf35c7836aae4fb5cbc4771b366216343e13
James Perrin <countrydawgg@gmail.com>
2022-07-11 06:58:39 -0700

    Add Prettier configuration.
    1. add .prettierrc.json file.
    2. add .prettierignore file.
    3. Formatted files.

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,16 +1,16 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-const CampsiteCare = (props) => {
-	return (
-		<Card>
-			<CardImg width='100%' src={props.campsite.image} alt={props.campsite.name} />
+const CampsiteCare = ({ campsite }) => {
+  const { image, name } = campsite;
 
-			<CardImgOverlay>
-				<CardTitle>{props.campsite.name}</CardTitle>
-			</CardImgOverlay>
-		</Card>
-	);
+  return (
+    <Card>
+      <CardImg width="100%" src={image} alt={name} />
+      <CardImgOverlay>
+        <CardTitle>{name}</CardTitle>
+      </CardImgOverlay>
+    </Card>
+  );
 };
-
 
 export default CampsiteCare;
--- src/features/campsites/CampsiteList.js
+++ src/features/campsites/CampsiteList.js
@@ -3,17 +3,17 @@ import { Col, Row } from 'reactstrap';
 import CampsiteCard from './CampsiteCard';
 
 const CampsitesList = () => {
-	return (
-		<Row className='ms-auto'>
-			{CAMPSITES.map((campsite) => {
-				return (
-					<Col md='5' className='m-4' key={campsite.id}>
-						<CampsiteCard campsite={campsite} />
-					</Col>
-				);
-			})}
-		</Row>
-	);
+  return (
+    <Row className="ms-auto">
+      {CAMPSITES.map((campsite) => {
+        return (
+          <Col md="5" className="m-4" key={campsite.id}>
+            <CampsiteCard campsite={campsite} />
+          </Col>
+        );
+      })}
+    </Row>
+  );
 };
 
 export default CampsitesList;
